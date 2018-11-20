398196bc4701127fd321570027ee2494d3d94081
ChanR <chan.rahar@gmail.om>
2018-11-19 19:47:38 -0500

    adding association

--- models/order.js
+++ models/order.js
@@ -1,17 +1,16 @@
-module.exports = function(sequelize, DataTypes) {
-    var Order = sequelize.define("Order", {
-      item: DataTypes.STRING,
-      price: DataTypes.DECIMAL(10,2),
-      quantity: DataTypes.INTEGER
-    });
+module.exports = function (sequelize, DataTypes) {
+  var Order = sequelize.define("Order", {
+    item: DataTypes.STRING,
+    price: DataTypes.DECIMAL(10, 2),
+    quantity: DataTypes.INTEGER
+  });
 
-    Order.associate = function(models) {
-      Order.belongsTo(models.Reservation, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
-    };
-    return Order;
+  Order.associate = function (models) {
+    Order.belongsTo(models.Reservation, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
   };
-  
+  return Order;
+};
